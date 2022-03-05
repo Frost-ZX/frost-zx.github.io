86dc2529346ddebd02245631ba20d4594c3f2675
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2022-03-05 19:27:27 +0800

    添加测试文章

--- public/config.js
+++ public/config.js
@@ -1,5 +1,18 @@
 var appConfig = {
 
-    contentList: [],
+    contentList: [
+        {
+            name: 'default',
+            label: '默认',
+            items: [
+                {
+                    name: 'test',
+                    title: '测试（Test）',
+                    createdAt: '2018-11-11',
+                    updatedAt: '2021-10-31'
+                },
+            ]
+        },
+    ],
 
 };
