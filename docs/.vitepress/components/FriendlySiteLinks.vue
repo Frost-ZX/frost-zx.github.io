bafaaed156632759aad6349b5145bf848ae6e3d5
Frost-ZX <frost-zx@foxmail.com>
2025-10-09 22:56:13 +0800

    refactor: 拆分友情链接列表到单独的文件

--- docs/.vitepress/components/FriendlySiteLinks.vue
+++ docs/.vitepress/components/FriendlySiteLinks.vue
@@ -1,189 +1,55 @@
 <script setup>
 import { withBase } from 'vitepress'
 import { ref, onBeforeMount } from 'vue';
+import { siteLinksOnline, siteLinksOffline } from '../site-links.mjs';
+
+/**
+ * @typedef  SiteItem
+ * @property {string} siteTitle
+ * @property {string} siteDesc
+ * @property {string} siteUrl
+ * @property {string} iconUrl0
+ * @property {string} iconUrl1
+ */
 
 /** 默认图标 */
 const defaultAvatar = withBase('/default-avatar.svg');
 
-/** 网站列表 - 正常访问 */
-const siteListOnline = ref([
+/**
+ * @desc 网站列表
+ * @type {ReturnType<typeof ref<SiteItem[]>>}
+ */
+const siteListOnline = ref([]);
 
-  // 博客
-  {
-    siteTitle: 'DSRBLOG',
-    siteDesc: 'blog.dsrkafuu.net',
-    siteUrl: 'https://blog.dsrkafuu.net/',
-    iconUrl0: 'https://c.frost-zx.top/data/uploads/blog/favicon/blog.dsrkafuu.net.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'haojunmei',
-    siteDesc: 'www.haojunmei.top',
-    siteUrl: 'https://www.haojunmei.top/',
-    iconUrl0: 'https://haojunmei.top/usr/themes/images/haojunmei.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: `Luminous' Home`,
-    siteDesc: '记录生活中的点滴',
-    siteUrl: 'https://luotianyi.vc/',
-    iconUrl0: 'https://static.lty.fun/weblogo/my.jpg',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'Panniyanjie 个人博客',
-    siteDesc: '追随科技的脚步从不会停止',
-    siteUrl: 'https://blog.wdztbil.top/',
-    iconUrl0: 'https://blog.wdztbil.top/usr/themes/Poem/images/favicon.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'qqAys',
-    siteDesc: '学途漫漫，朋辈同行',
-    siteUrl: 'https://qqays.xyz/',
-    iconUrl0: 'https://c.frost-zx.top/data/uploads/blog/favicon/qqays.xyz.webp',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: `Saiita's Blog`,
-    siteDesc: '好记性不如烂笔头',
-    siteUrl: 'https://www.saiita.com.cn/',
-    iconUrl0: 'https://c.frost-zx.top/data/uploads/blog/favicon/www.saiita.com.cn.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'zgcwkj',
-    siteDesc: '做老实人，说老实话，干老实事，就是实事求是！',
-    siteUrl: 'https://zgcwkj.cn/',
-    iconUrl0: 'https://zgcwkj.cn/favicon.ico',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '冰屋小站',
-    siteDesc: 'blog.biduang.cn',
-    siteUrl: 'https://blog.biduang.cn/',
-    iconUrl0: 'https://gravatar.loli.net/avatar/5273a4d195a45fd8b900545f63a94f58?s=200&r=G',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '高坂滑稽果的滑稽果农场',
-    siteDesc: '一个萌新awa',
-    siteUrl: 'https://funny233.xyz/',
-    iconUrl0: 'https://funny233.xyz/favicon.ico',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '派兹的小站',
-    siteDesc: '驶向未来之船',
-    siteUrl: 'https://blog.blw.moe/',
-    iconUrl0: 'https://global.cdn.blingwang.cn/blog/head.jpg',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '王鑫的小站',
-    siteDesc: '活着就是为了折腾！',
-    siteUrl: 'https://wxweb.xyz/',
-    iconUrl0: 'https://pic.wxweb.xyz/pic/favicon.ico',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '御坂秋生の小窝',
-    siteDesc: '不努力就只能听到别人的好消息',
-    siteUrl: 'https://www.akio.top/',
-    iconUrl0: 'https://webstatic.akio.top/user/NiuBoss123.jpg',
-    iconUrl1: '',
-  },
+/**
+ * @desc 网站列表
+ * @type {ReturnType<typeof ref<SiteItem[]>>}
+ */
+const siteListOffline = ref([]);
 
-  // Minecraft
-  {
-    siteTitle: '麦子茶会服务器',
-    siteDesc: '一个多元素的 Minecraft 服务器',
-    siteUrl: 'https://www.wheat-server.top/',
-    iconUrl0: 'https://www.wheat-server.top/upload/system/favicon.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '氢气工艺服务器',
-    siteDesc: '氢气工艺，一个以多元化发展为主的多模组创造类服务器',
-    siteUrl: 'https://wiki.hydcraft.cn/',
-    iconUrl0: 'https://wiki.hydcraft.cn/favicon/logo.png',
-    iconUrl1: '',
-  },
+/** 初始化数据 */
+function initData() {
 
-]);
+  siteListOnline.value = siteLinksOnline.map((item) => {
+    return {
+      siteTitle: item.siteTitle,
+      siteDesc: item.siteDesc,
+      siteUrl: item.siteUrl,
+      iconUrl0: item.iconUrl,
+      iconUrl1: '',
+    };
+  });
 
-/** 网站列表 - 无法访问 */
-const siteListOffline = ref([
+  siteListOffline.value = siteLinksOffline.map((item) => {
+    return {
+      siteTitle: item.siteTitle,
+      siteDesc: item.siteDesc,
+      siteUrl: item.siteUrl,
+      iconUrl0: item.iconUrl,
+      iconUrl1: '',
+    };
+  });
 
-  // 无法访问
-  {
-    siteTitle: '4SSH',
-    siteDesc: '4ssh.cn',
-    siteUrl: 'https://4ssh.cn/',
-    iconUrl0: '',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'AurLemon Blog',
-    siteDesc: 'Aurora_Lemon 的个人博客，记录个人感想、生活、记录。',
-    siteUrl: 'https://blog.aurlemon.top/',
-    iconUrl0: 'https://blog.aurlemon.top/favicon.ico',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'LuRenDing',
-    siteDesc: 'blog.lurending.top',
-    siteUrl: 'https://blog.lurending.top/',
-    iconUrl0: 'https://blog.lurending.top/favicon.ico',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: 'UNKNOW date',
-    siteDesc: 'n-a.date',
-    siteUrl: 'https://n-a.date/',
-    iconUrl0: 'https://n-a.date/favicon.ico',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '安和Blog',
-    siteDesc: 'nekocoffee.com',
-    siteUrl: 'https://nekocoffee.com/',
-    iconUrl0: 'https://i.imgtg.com/2022/06/23/7Sg4L.jpg',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '任我乐',
-    siteDesc: '一条迷茫的死咸鱼。',
-    siteUrl: 'https://renwole.com/',
-    iconUrl0: 'https://renwole.com/wp-content/uploads/2017/04/cropped-Renwole-logo-192x192-192x192.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '软件探索',
-    siteDesc: '分享好用的软件和网站',
-    siteUrl: 'https://www.appts.cn/',
-    iconUrl0: 'https://cdn.jsdelivr.net/gh/appexplore/cdn/appts@v1.0/img/logo.png',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '小哈博客',
-    siteDesc: '一只硬核的小哈，欢迎访问',
-    siteUrl: 'https://blog.littleha.cn/',
-    iconUrl0: 'https://www.gravatar.com/avatar/baafed38e0ebbe171caa1cec177bf82e',
-    iconUrl1: '',
-  },
-  {
-    siteTitle: '云影云烟工作室',
-    siteDesc: 'www.yunyingstudio.cn',
-    siteUrl: 'https://www.yunyingstudio.cn/',
-    iconUrl0: 'https://www.yunyingstudio.cn/logo.jpg',
-    iconUrl1: '',
-  },
-
-]);
-
-/** 加载链接项中的图标 */
-function loadIcons() {
   ((dataList) => {
     dataList.forEach((siteList) => {
       siteList.forEach((item) => {
@@ -203,6 +69,7 @@ function loadIcons() {
     siteListOnline.value,
     siteListOffline.value,
   ]);
+
 }
 
 /**
@@ -214,7 +81,7 @@ function openLink(item) {
 }
 
 onBeforeMount(() => {
-  loadIcons();
+  initData();
 });
 </script>
 
---
+++ docs/.vitepress/site-links.mjs
@@ -0,0 +1,167 @@
+/** 网站列表 - 正常访问 */
+export const siteLinksOnline = [
+
+  // 博客
+  {
+    siteTitle: 'DSRBLOG',
+    siteDesc: 'blog.dsrkafuu.net',
+    siteUrl: 'https://blog.dsrkafuu.net/',
+    iconUrl: 'https://c.frost-zx.top/data/uploads/blog/favicon/blog.dsrkafuu.net.png',
+    isGlobal: false,
+  },
+  {
+    siteTitle: 'haojunmei',
+    siteDesc: 'www.haojunmei.top',
+    siteUrl: 'https://www.haojunmei.top/',
+    iconUrl: 'https://haojunmei.top/usr/themes/images/haojunmei.png',
+    isGlobal: true,
+  },
+  {
+    siteTitle: `Luminous' Home`,
+    siteDesc: '记录生活中的点滴',
+    siteUrl: 'https://luotianyi.vc/',
+    iconUrl: 'https://static.lty.fun/weblogo/my.jpg',
+    isGlobal: false,
+  },
+  {
+    siteTitle: 'Panniyanjie 个人博客',
+    siteDesc: '追随科技的脚步从不会停止',
+    siteUrl: 'https://blog.wdztbil.top/',
+    iconUrl: 'https://blog.wdztbil.top/usr/themes/Poem/images/favicon.png',
+    isGlobal: false,
+  },
+  {
+    siteTitle: 'qqAys',
+    siteDesc: '学途漫漫，朋辈同行',
+    siteUrl: 'https://qqays.xyz/',
+    iconUrl: 'https://c.frost-zx.top/data/uploads/blog/favicon/qqays.xyz.webp',
+    isGlobal: false,
+  },
+  {
+    siteTitle: `Saiita's Blog`,
+    siteDesc: '好记性不如烂笔头',
+    siteUrl: 'https://www.saiita.com.cn/',
+    iconUrl: 'https://c.frost-zx.top/data/uploads/blog/favicon/www.saiita.com.cn.png',
+    isGlobal: false,
+  },
+  {
+    siteTitle: 'zgcwkj',
+    siteDesc: '做老实人，说老实话，干老实事，就是实事求是！',
+    siteUrl: 'https://zgcwkj.cn/',
+    iconUrl: 'https://zgcwkj.cn/favicon.ico',
+    isGlobal: false,
+  },
+  {
+    siteTitle: '冰屋小站',
+    siteDesc: 'blog.biduang.cn',
+    siteUrl: 'https://blog.biduang.cn/',
+    iconUrl: 'https://gravatar.loli.net/avatar/5273a4d195a45fd8b900545f63a94f58?s=200&r=G',
+    isGlobal: false,
+  },
+  {
+    siteTitle: '高坂滑稽果的滑稽果农场',
+    siteDesc: '一个萌新awa',
+    siteUrl: 'https://funny233.xyz/',
+    iconUrl: 'https://funny233.xyz/favicon.ico',
+    isGlobal: false,
+  },
+  {
+    siteTitle: '派兹的小站',
+    siteDesc: '驶向未来之船',
+    siteUrl: 'https://blog.blw.moe/',
+    iconUrl: 'https://global.cdn.blingwang.cn/blog/head.jpg',
+    isGlobal: false,
+  },
+  {
+    siteTitle: '王鑫的小站',
+    siteDesc: '活着就是为了折腾！',
+    siteUrl: 'https://wxweb.xyz/',
+    iconUrl: 'https://pic.wxweb.xyz/pic/favicon.ico',
+    isGlobal: false,
+  },
+  {
+    siteTitle: '御坂秋生の小窝',
+    siteDesc: '不努力就只能听到别人的好消息',
+    siteUrl: 'https://www.akio.top/',
+    iconUrl: 'https://webstatic.akio.top/user/NiuBoss123.jpg',
+    isGlobal: false,
+  },
+
+  // Minecraft
+  {
+    siteTitle: '麦子茶会服务器',
+    siteDesc: '一个多元素的 Minecraft 服务器',
+    siteUrl: 'https://www.wheat-server.top/',
+    iconUrl: 'https://www.wheat-server.top/upload/system/favicon.png',
+    isGlobal: false,
+  },
+  {
+    siteTitle: '氢气工艺服务器',
+    siteDesc: '氢气工艺，一个以多元化发展为主的多模组创造类服务器',
+    siteUrl: 'https://wiki.hydcraft.cn/',
+    iconUrl: 'https://wiki.hydcraft.cn/favicon/logo.png',
+    isGlobal: false,
+  },
+
+];
+
+/** 网站列表 - 无法访问 */
+export const siteLinksOffline = [
+
+  // 无法访问
+  {
+    siteTitle: '4SSH',
+    siteDesc: '4ssh.cn',
+    siteUrl: 'https://4ssh.cn/',
+    iconUrl: '',
+  },
+  {
+    siteTitle: 'AurLemon Blog',
+    siteDesc: 'Aurora_Lemon 的个人博客，记录个人感想、生活、记录。',
+    siteUrl: 'https://blog.aurlemon.top/',
+    iconUrl: 'https://blog.aurlemon.top/favicon.ico',
+  },
+  {
+    siteTitle: 'LuRenDing',
+    siteDesc: 'blog.lurending.top',
+    siteUrl: 'https://blog.lurending.top/',
+    iconUrl: 'https://blog.lurending.top/favicon.ico',
+  },
+  {
+    siteTitle: 'UNKNOW date',
+    siteDesc: 'n-a.date',
+    siteUrl: 'https://n-a.date/',
+    iconUrl: 'https://n-a.date/favicon.ico',
+  },
+  {
+    siteTitle: '安和Blog',
+    siteDesc: 'nekocoffee.com',
+    siteUrl: 'https://nekocoffee.com/',
+    iconUrl: 'https://i.imgtg.com/2022/06/23/7Sg4L.jpg',
+  },
+  {
+    siteTitle: '任我乐',
+    siteDesc: '一条迷茫的死咸鱼。',
+    siteUrl: 'https://renwole.com/',
+    iconUrl: 'https://renwole.com/wp-content/uploads/2017/04/cropped-Renwole-logo-192x192-192x192.png',
+  },
+  {
+    siteTitle: '软件探索',
+    siteDesc: '分享好用的软件和网站',
+    siteUrl: 'https://www.appts.cn/',
+    iconUrl: 'https://cdn.jsdelivr.net/gh/appexplore/cdn/appts@v1.0/img/logo.png',
+  },
+  {
+    siteTitle: '小哈博客',
+    siteDesc: '一只硬核的小哈，欢迎访问',
+    siteUrl: 'https://blog.littleha.cn/',
+    iconUrl: 'https://www.gravatar.com/avatar/baafed38e0ebbe171caa1cec177bf82e',
+  },
+  {
+    siteTitle: '云影云烟工作室',
+    siteDesc: 'www.yunyingstudio.cn',
+    siteUrl: 'https://www.yunyingstudio.cn/',
+    iconUrl: 'https://www.yunyingstudio.cn/logo.jpg',
+  },
+
+];
