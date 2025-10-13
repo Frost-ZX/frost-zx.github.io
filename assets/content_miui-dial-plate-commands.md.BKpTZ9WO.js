d9a47fb8f5d0bdb2b694b57637df4f858a71c8f4
Frost-ZX <frost-zx@foxmail.com>
2025-10-13 10:24:40 +0800

    更新内容（2025-10-12）

---
+++ assets/content_miui-dial-plate-commands.md.BKpTZ9WO.js
@@ -0,0 +1,16 @@
+import{_ as s,c as n,o as p,ah as e}from"./chunks/framework.ByubJJ5I.js";const h=JSON.parse('{"title":"MIUI 拨号盘指令（代码）合集","description":"","frontmatter":{"title":"MIUI 拨号盘指令（代码）合集","date":"2025-02-11T20:55:13.000Z","lastmod":"2025-02-20T22:58:12.000Z","tags":["Android","小米","系统","HyperOS","MIUI"]},"headers":[],"relativePath":"content/miui-dial-plate-commands.md","filePath":"content/miui-dial-plate-commands.md"}'),i={name:"content/miui-dial-plate-commands.md"};function l(t,a,c,o,r,d){return p(),n("div",null,[...a[0]||(a[0]=[e(`<h1 id="miui-拨号盘指令-代码-合集" tabindex="-1">MIUI 拨号盘指令（代码）合集 <a class="header-anchor" href="#miui-拨号盘指令-代码-合集" aria-label="Permalink to “MIUI 拨号盘指令（代码）合集”">​</a></h1><p>参考：<a href="https://www.coolapk.com/feed/50745332?shareKey=YWVjMDU3MjRjNGFhNjU0NTg5MGM~&amp;shareUid=1033889" target="_blank" rel="noreferrer">www.coolapk.com</a></p><p>部分指令也支持在澎湃 OS 上使用</p><div class="language-text"><button title="Copy Code" class="copy"></button><span class="lang">text</span><pre class="shiki shiki-themes github-light github-dark" style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;--shiki-light-bg:#fff;--shiki-dark-bg:#24292e;" tabindex="0" dir="ltr"><code><span class="line"><span>*#*#54638#*#* 显示 5G 开关</span></span>
+<span class="line"><span>*#*#8667#*#* 显示 VoNR 开关</span></span>
+<span class="line"><span>*#*#86583#*#* 显示 VoLTE 选项</span></span>
+<span class="line"><span>*#*#869434#*#* 显示 VoWiFi 选项</span></span>
+<span class="line"><span>*#*#726633#*#* 开启 5G 网络模式选择</span></span>
+<span class="line"><span>*#*#4636#*#* 手机信息锁网（查看手机信号强弱）</span></span>
+<span class="line"><span>*#*#6484#*#* cit 工程模式</span></span>
+<span class="line"><span>*#*#64663#*#* 手机功能测试</span></span>
+<span class="line"><span>*#*#3223#*#* 开启 DC 调光选项</span></span>
+<span class="line"><span>*#*#284#*#* 生成 bug 报告（可查看手机电池剩余容量）</span></span>
+<span class="line"><span>*#*#6485#*#* 充电与电池相关的信息</span></span>
+<span class="line"><span>*#*#76937#*#* 神隐模式</span></span>
+<span class="line"><span>*#*#37263#*#* 帧率显示</span></span>
+<span class="line"><span>*#*#3646633#*#* MTK 工程模式（可用于锁频段）</span></span>
+<span class="line"><span>*#*#6666#*#* 退出演示模式（仅展示机可用）</span></span>
+<span class="line"><span>*#06# 移动识别码 IMEI号</span></span></code></pre></div>`,4)])])}const _=s(i,[["render",l]]);export{h as __pageData,_ as default};
